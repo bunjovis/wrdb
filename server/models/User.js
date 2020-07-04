5497140a88916b59fd52c2b394e75602bb322f44
Kari Harmaahieta <kari.harmaahieta@gmail.com>
2020-07-04 08:50:04 +0000

    User Model ready

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 1,
+    set: (password) => {
+      return !password || password.length === 0
+        ? password
+        : bcrypt.hashSync(password, 10);
+    },
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
