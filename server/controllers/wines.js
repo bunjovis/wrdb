fc66a512d5e1800d69d35b1b6a3e2ced858cdf04
Kari Harmaahieta <kari.harmaahieta@gmail.com>
2020-08-13 20:46:46 +0300

    Change wine ingredients and controllers adding

--- server/controllers/wines.js
+++ server/controllers/wines.js
@@ -1,4 +1,6 @@
 const Wine = require('../models/Wine');
+const Ingredient = require('../models/Ingredient').model;
+const Comment = require('../models/Comment').model;
 
 const listWines = (req, res) => {
   Wine.find((err, res2) => res.status(200).json({ wines: res2 }));
@@ -17,39 +19,109 @@ const addWine = (req, res) => {
     comments,
     labelId,
   } = req.body;
-  const newWine = {
-    name,
-    ingredients,
-    totalCost,
-    startingGravity,
-    startingVolume,
-  };
-  if (finalGravity) {
-    newWine.finalGravity = finalGravity;
+  if (ingredients) {
+    const newIngredients = [];
+    const len = ingredients.length;
+    let i = 0;
+    ingredients.forEach((ingredient) => {
+      const newIngredient = new Ingredient({
+        type: ingredient.type,
+        amount: ingredient.amount,
+        comment: ingredient.comment,
+      });
+      newIngredient.save((err, prod) => {
+        if (err) {
+          return res.status(500).json({
+            message: 'Error occured',
+            error: err,
+          });
+        } else {
+          newIngredients.push(prod);
+          i++;
+          if (i == len) {
+            const newWine = {
+              name,
+              ingredients: newIngredients,
+              totalCost,
+              startingGravity,
+              startingVolume,
+            };
+            if (finalGravity) {
+              newWine.finalGravity = finalGravity;
+            }
+            if (finalVolume) {
+              newWine.finalVolume = finalVolume;
+            }
+            if (alcoholContent) {
+              newWine.alcoholContent = alcoholContent;
+            }
+            if (bottlingDate) {
+              newWine.bottlingDate = bottlingDate;
+            }
+            if (comments) {
+              const newComments = [];
+              const len2 = comments.length;
+              let j = 0;
+              comments.forEach((comment) => {
+                const newComment = new Comment({
+                  text: comment.text,
+                });
+                newComment.save((err2, prod2) => {
+                  if (err2) {
+                    return res.status(500).json({
+                      message: 'Error occured',
+                      error: err2,
+                    });
+                  } else {
+                    newComments.push(prod2);
+                    j++;
+                    if (j == len2) {
+                      newWine.comments = newComments;
+                      if (labelId) {
+                        newWine.labelId = labelId;
+                      }
+                      const wine = new Wine(newWine);
+                      wine.save((err3, doc) => {
+                        if (err3 || !doc) {
+                          return res
+                            .status(500)
+                            .json({ message: 'Error occured', error: err3 });
+                        } else {
+                          return res
+                            .status(201)
+                            .json({ message: 'New wine created', wine: doc });
+                        }
+                      });
+                    }
+                  }
+                });
+              });
+            } else {
+              if (labelId) {
+                newWine.labelId = labelId;
+              }
+              const wine = new Wine(newWine);
+              wine.save((err3, doc) => {
+                if (err3 || !doc) {
+                  return res
+                    .status(500)
+                    .json({ message: 'Error occured', error: err3 });
+                } else {
+                  return res
+                    .status(201)
+                    .json({ message: 'New wine created', wine: doc });
+                }
+              });
+            }
+          }
+        }
+      });
+    });
+  } else {
+    return res.status(500).json({
+      message: 'Error occured',
+    });
   }
-  if (finalVolume) {
-    newWine.finalVolume = finalVolume;
-  }
-  if (alcoholContent) {
-    newWine.alcoholContent = alcoholContent;
-  }
-  if (bottlingDate) {
-    newWine.bottlingDate = bottlingDate;
-  }
-  if (comments) {
-    newWine.comments = comments;
-  }
-  if (labelId) {
-    newWine.labelId = labelId;
-  }
-  const wine = new Wine(newWine);
-  wine.save((err, doc) => {
-    if (err || !doc) {
-      return res.status(500).json({ message: 'Error occured', error: err });
-    } else {
-      return res.status(201).json({ message: 'New wine created', wine: doc });
-    }
-  });
 };
 const showWine = (req, res) => {
   const { id } = req.params;
@@ -94,7 +166,18 @@ const editWine = (req, res) => {
     }
     if (ingredients) {
       if (Array.isArray(ingredients)) {
-        doc.ingredients = ingredients;
+        const newIngredients = [];
+        for (ingredient in ingredients) {
+          const newIngredient = new Ingredient({
+            type: ingredient.type,
+            amount: ingredient.amount,
+            comment: ingredient.comment,
+          });
+          newIngredient.save((err, prod) => {
+            newIngredients.push(prod);
+          });
+        }
+        doc.ingredients = newIngredients;
       } else {
         return res.status(500).json({ message: 'Error occured', error: err });
       }
@@ -133,18 +216,46 @@ const editWine = (req, res) => {
     if (bottlingDate) {
       doc.bottlingDate = bottlingDate;
     }
+
     if (comments) {
-      doc.comments = comments;
-    }
-    if (labelId) {
-      doc.labelId = labelId;
-    }
-    doc.save({ runValidators: true }, (err2, doc2) => {
-      if (err2) {
-        return res.status(500).json({ message: 'Error occured', error: err2 });
+      const newComments = [];
+      const len2 = comments.length;
+      const j = 0;
+      comments.forEach((comment) => {
+        const newComment = new Comment({
+          text: comment.text,
+        });
+        newComment.save((err2, prod2) => {
+          newComments.push(prod2);
+          j++;
+          if (j == len2) {
+            if (labelId) {
+              doc.labelId = labelId;
+            }
+            doc.save({ runValidators: true }, (err3, doc3) => {
+              if (err2) {
+                return res
+                  .status(500)
+                  .json({ message: 'Error occured', error: err3 });
+              }
+              return res.status(200).json({ wine: doc3 });
+            });
+          }
+        });
+      });
+    } else {
+      if (labelId) {
+        doc.labelId = labelId;
       }
-      return res.status(200).json({ wine: doc2 });
-    });
+      doc.save({ runValidators: true }, (err2, doc2) => {
+        if (err2) {
+          return res
+            .status(500)
+            .json({ message: 'Error occured', error: err2 });
+        }
+        return res.status(200).json({ wine: doc2 });
+      });
+    }
   });
 };
 const deleteWine = (req, res) => {
