d83d89a5cfe3b97dc1606a06b20addf754b7d6c2
Kari Harmaahieta <kari.harmaahieta@gmail.com>
2020-08-23 13:10:48 +0300

    Fix missing comma

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve('client', 'build', 'index.html'));
 });
 
-app.listen(port, '0.0.0.0' () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Listening on port ${port}`);
 });
 
